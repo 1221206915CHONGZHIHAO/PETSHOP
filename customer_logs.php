f7451eff4a02c6c467b690e048ab8eb63b9017c1
CHONG ZHI HAO <1221206915@student.mmu.edu.my>
2025-04-29 19:55:39 +0800

    some changes

--- customer_logs.php
+++ customer_logs.php
@@ -88,7 +88,7 @@ $conn->close();
                         <a class="nav-link text-light" data-bs-toggle="collapse" href="#customerMenu">
                             <i class="fas fa-user-friends me-2"></i>Customer Management
                         </a>
-                        <div class="collapse" id="customerMenu">
+                        <div class="collapse show" id="customerMenu">
                             <ul class="nav flex-column ps-4">
                                 <li class="nav-item">
                                     <a class="nav-link text-light" href="customer_list.php">
@@ -96,7 +96,7 @@ $conn->close();
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link text-light" href="customer_logs.php">
+                                    <a class="nav-link text-light active" href="customer_logs.php">
                                         <i class="fas fa-history me-2"></i>Login/Logout Logs
                                     </a>
                                 </li>
