f9f0d16d81b5d61ee4310f3002415ec9bddb39fe
DAVID HI ZHE YA <1221205623@student.mmu.edu.my>
2025-05-07 08:51:38 +0800

    update

--- contact_us.php
+++ contact_us.php
@@ -268,7 +268,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         </div>
                         <p>Your trusted partner in pet product. We're dedicated to providing quality products for pet lovers everywhere.</p>
                         <div class="social-links">
-                            <a href="#"><i class="bi bi-facebook"></i></a>
+                            <a href="https://www.facebook.com/profile.php?id=61575717095389"><i class="bi bi-facebook"></i></a>
                             <a href="#"><i class="bi bi-instagram"></i></a>
                         </div>
                     </div>
