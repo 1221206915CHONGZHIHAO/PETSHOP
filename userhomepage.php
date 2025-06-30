97d80a8a6d4007841112aac864abb443623a8fee
CHONG ZHI HAO <1221206915@student.mmu.edu.my>
2025-06-30 22:44:05 +0800

    update

--- userhomepage.php
+++ userhomepage.php
@@ -33,7 +33,7 @@ if (isset($_SESSION['customer_id'])) {
 
 // Fetch best sellers based on the 'best-selling' logic (most recently updated)
 $best_sellers = [];
-$best_sellers_sql = "SELECT * FROM products ORDER BY updated_at DESC LIMIT 4";
+$best_sellers_sql = "SELECT * FROM products WHERE stock_quantity > 0 ORDER BY updated_at DESC LIMIT 4";
 $best_sellers_result = $conn->query($best_sellers_sql);
 if ($best_sellers_result && $best_sellers_result->num_rows > 0) {
     while($row = $best_sellers_result->fetch_assoc()) {
